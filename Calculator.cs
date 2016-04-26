59fd6be7f21919906b9aa9a0ec4672371a1bab01
Sergey Kalitin <sergey.kalitin@gmail.com>
2016-04-27 00:10:02 +0300

    Initial commit

---
+++ Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fixtures
+{
+    public class Calculator
+    {
+        public int Foo { get; set; }
+        public int Bar { get; set; }
+        public int Result()
+        {
+            return Foo * 2 + Bar + 1;
+        }
+    }
+}
